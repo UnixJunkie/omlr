91f6bf5122714f5893662f669d9a4b1123033aae
Francois Berenger <unixjunkie@sdf.org>
2020-09-02 11:36:00 +0900

    MLR: broke load_csv_file into read_csv_file and matrix_of_csv_lines

--- src/MLR.ml
+++ src/MLR.ml
@@ -7,39 +7,6 @@ module S = BatString
 
 open Printf
 
-(* load all lines from given CSV file
-   [skip_header] allows to ignore the first line (CSV header), if necessary.
-   The first column is supposed to contain the target variable.
-   All other columns are supposed to contain the parameters whose weights
-   in the MLR model we want to find. *)
-let load_csv_file ~randomize ~skip_header sep csv_fn =
-  let all_lines =
-    let all_lines' = match Utls.lines_of_file csv_fn with
-      | [] -> failwith ("MLR.load_csv_file: no lines in " ^ csv_fn)
-      | x :: xs -> if skip_header then xs else x :: xs in
-    if randomize then
-      let rng = BatRandom.State.make_self_init () in
-      L.shuffle ~state:rng all_lines'
-    else
-      all_lines' in
-  let fst_line = L.hd all_lines in
-  let dimx = (S.count_char fst_line sep) + 1 in
-  let dimy = L.length all_lines in
-  let res = A.make_matrix dimx dimy 0.0 in
-  L.iteri (fun y line ->
-      let tokens = L.map float_of_string (S.split_on_char sep line) in
-      let n = L.length tokens in
-      (if n <> dimx then
-         failwith
-           (sprintf "MLR.load_csv_file: file %s line %d has %d features \
-                     instead of %d" csv_fn y n dimx)
-      );
-      L.iteri (fun x feat ->
-          res.(x).(y) <- feat
-        ) tokens
-    ) all_lines;
-  res
-
 type std_param = { mean: float;
                    sd: float }
 
@@ -50,6 +17,40 @@ type model_param = { mean: float;
                      w: float }
 
 type model = model_param array
+
+(* load all lines from given CSV file
+   [skip_header] allows to ignore the first line (CSV header), if necessary.
+   The first column is supposed to contain the target variable.
+   All other columns are supposed to contain the parameters whose weights
+   in the MLR model we want to find. *)
+let read_csv_file ~randomize ~skip_header csv_fn =
+  let all_lines' = match Utls.lines_of_file csv_fn with
+    | [] -> failwith ("MLR.load_csv_file: no lines in " ^ csv_fn)
+    | x :: xs -> if skip_header then xs else x :: xs in
+  if randomize then
+    let rng = BatRandom.State.make_self_init () in
+    L.shuffle ~state:rng all_lines'
+  else
+    all_lines'
+
+(* CSV lines to 2D array *)
+let matrix_of_csv_lines sep all_lines =
+  let fst_line = L.hd all_lines in
+  let dimx = (S.count_char fst_line sep) + 1 in
+  let dimy = L.length all_lines in
+  let res = A.make_matrix dimx dimy 0.0 in
+  L.iteri (fun y line ->
+      let tokens = L.map float_of_string (S.split_on_char sep line) in
+      let n = L.length tokens in
+      (if n <> dimx then
+         failwith (sprintf "MLR.matrix_of_csv_lines: features: %d <> %d: %s"
+                     n dimx line)
+      );
+      L.iteri (fun x feat ->
+          res.(x).(y) <- feat
+        ) tokens
+    ) all_lines;
+  res
 
 (* compute the standardization parameters *)
 let standardization_params arr =
