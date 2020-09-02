435087c56a59bf02542098baaf49044645bf7f79
Francois Berenger <unixjunkie@sdf.org>
2020-09-02 15:47:31 +0900

    Model: implemented --NxCV

--- src/model.ml
+++ src/model.ml
@@ -41,6 +41,29 @@ let test sep model nb_test test_lines =
     A.to_list predicted' in
   (actual, preds)
 
+let train_test debug sep randomize skip_header train_portion maybe_model_fn input_fn =
+  let train_lines, test_lines =
+    let all_lines = MLR.read_csv_file ~randomize ~skip_header input_fn in
+    Cpm.Utls.train_test_split train_portion all_lines in
+  let nb_train, nb_test = L.(length train_lines, length test_lines) in
+  Log.info "train: %d test: %d total: %d"
+    nb_train nb_test (nb_train + nb_test);
+  (* train *)
+  let model = train debug sep maybe_model_fn train_lines in
+  (* test *)
+  test sep model nb_test test_lines
+
+let train_test_nfolds debug sep randomize skip_header nfolds input_fn =
+  let all_lines = MLR.read_csv_file ~randomize ~skip_header input_fn in
+  let train_tests = Cpm.Utls.cv_folds nfolds all_lines in
+  let actual_preds =
+    L.map (fun (train_lines, test_lines) ->
+        let model = train debug sep None train_lines in
+        test sep model (L.length test_lines) test_lines
+      ) train_tests in
+  let actual, preds = L.split actual_preds in
+  (L.concat actual, L.concat preds)
+
 let main () =
   Log.(set_log_level DEBUG);
   Log.color_on ();
@@ -52,7 +75,7 @@ let main () =
       eprintf "usage:\n\
                %s\n  \
                [-i <input.csv>]: input CSV file\n  \
-               TODO [--NxCV <int>]: number of folds of cross validation\n  \
+               [--NxCV <int>]: number of folds of cross validation\n  \
                [-s|--save <filename>]: save model to file\n  \
                TODO [-l|--load <filename>]: restore model from file\n  \
                TODO [-o <filename>]: predictions output file\n  \
@@ -73,22 +96,16 @@ let main () =
   let maybe_model_fn = CLI.get_string_opt ["-s"] args in
   let debug = CLI.get_set_bool ["-v"] args in
   let sep = CLI.get_char_def ["-d"] args ',' in
-  let _nfolds = CLI.get_int_def ["-n";"--NxCV"] args 1 in
-  (* TODO implement --NxCV *)
+  let nfolds = CLI.get_int_def ["-n";"--NxCV"] args 1 in
   (* TODO implement -l *)
-  (* TODO implement -s *)
   let input_fn = CLI.get_string ["-i"] args in
   CLI.finalize (); (* ------------------------------------------------------ *)
-  let train_lines, test_lines =
-    let all_lines = MLR.read_csv_file ~randomize ~skip_header input_fn in
-    Cpm.Utls.train_test_split train_portion all_lines in
-  let nb_train, nb_test = L.(length train_lines, length test_lines) in
-  Log.info "train: %d test: %d total: %d"
-    nb_train nb_test (nb_train + nb_test);
-  (* train *)
-  let model = train debug sep maybe_model_fn train_lines in
-  (* test *)
-  let actual, preds = test sep model nb_test test_lines in
+  let actual, preds =
+    if nfolds <= 1 then
+      train_test debug sep randomize skip_header train_portion maybe_model_fn input_fn
+    else (* nfolds > 1 *)
+      train_test_nfolds debug sep randomize skip_header nfolds input_fn
+  in
   let r2 = Cpm.RegrStats.r2 actual preds in
   let r2_str = sprintf "R2: %.3f" r2 in
   (if not no_plot then Gnuplot.regr_plot r2_str actual preds);
